d32a89c4c57ad58fe8e28f28ccfc55e9650ec3fc
Rusty Walters <rusty.walters@gmail.com>
2016-01-18 18:09:24 -0600

    worked through adding form and CSS

--- src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -2,9 +2,15 @@ package com.caveofprogramming.spring.web.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,15 +30,33 @@ public class OffersController {
 
 	@RequestMapping("/offers")
 	public String showOffers(Model model) {
+		//offersService.throwTestException();
+		
 		List<Offer> offers = offersService.getCurrent();
 		model.addAttribute("offers", offers);
 		return "offers";
 	}
+	
+//	@ExceptionHandler(DataAccessException.class)
+//	public String handleDatabaseException(DataAccessException ex) {
+//		return "error";
+//	}
 
 	@RequestMapping("/createoffer")
-	public String createOffer() {
+	public String createOffer(Model model) {
+		model.addAttribute("offer", new Offer());
 		return "createoffer";
 	}
+	
+	@RequestMapping(value = "/docreate", method = RequestMethod.POST)
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		if(result.hasErrors()) {
+			return "createoffer";
+		} 
+		
+		offersService.create(offer);
+		return "offercreated";
+	}	
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String showTest(Model model, @RequestParam("id") String id) {
--- src/com/caveofprogramming/spring/web/dao/Offer.java
+++ src/com/caveofprogramming/spring/web/dao/Offer.java
@@ -1,9 +1,23 @@
 package com.caveofprogramming.spring.web.dao;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.caveofprogramming.spring.web.validation.ValidEmail;
+
 public class Offer {
 	private int id;
+	
+	@Size(min=5, max=100, message="Name must be between 5 and 100 characters.")
 	private String name;
+	
+	@NotNull
+	//@Pattern(regexp=".*\\@.*\\..*", message="This does not appear to be a valid email address.")
+	@ValidEmail(min=6, message="This email address is not valid.")
 	private String email;
+	
+	@Size(min=20, max=255, message="Text must be between 20 and 255 characters.")
 	private String text;
 
 	public Offer() {
